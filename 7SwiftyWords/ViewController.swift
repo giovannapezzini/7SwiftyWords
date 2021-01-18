f558d4fdb237088b146443cb3b3477e31aae2752
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-18 15:39:20 -0300

    Add letterTapped and clearTapped methods

--- 7SwiftyWords/ViewController.swift
+++ 7SwiftyWords/ViewController.swift
@@ -132,7 +132,11 @@ class ViewController: UIViewController {
     }
     
     @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
         
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
     }
     
     @objc func submitTapped(_ sender: UIButton) {
@@ -140,7 +144,13 @@ class ViewController: UIViewController {
     }
     
     @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
         
+        for button in activatedButtons {
+            button.isHidden = false
+        }
+        
+        activatedButtons.removeAll()
     }
     
     func loadLevel() {
